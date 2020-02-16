c728570a4e21182b5135711521dd9f1a33bf5df9
Christopher Giroir <kelsin@valefor.com>
2020-02-15 23:00:27 -0800

    Slightly better abstraction

--- src/reducers/company.ts
+++ src/reducers/company.ts
@@ -4,14 +4,16 @@ import { reduce } from 'ramda'
 import { Company, Private } from '18dux/types'
 import { PAY_PRIVATES } from '18dux/actions'
 
+const privateRevenue = (company:Company):number =>
+  reduce((total:number, priv:Private) =>
+    total + priv.revenue, 0, company.privates)
+
 const companyReducer = (company:Company, action:AnyAction):Company => {
   switch (action.type) {
   case PAY_PRIVATES:
     return {
       ...company,
-      cash: company.cash + reduce((total:number, priv:Private) => {
-        return total + priv.revenue
-      }, 0, company.privates)
+      cash: company.cash + privateRevenue(company)
     }
   default:
     return company
--- src/reducers/player.ts
+++ src/reducers/player.ts
@@ -6,14 +6,16 @@ import { Player, Private } from '18dux/types'
 
 import { PAY_PRIVATES } from '18dux/actions'
 
+const privateRevenue = (player:Player):number =>
+  reduce((total:number, priv:Private) =>
+    total + priv.revenue, 0, player.privates)
+
 const playerReducer = (player:Player, action:AnyAction):Player => {
   switch (action.type) {
   case PAY_PRIVATES:
     return {
       ...player,
-      cash: player.cash + reduce((total:number, priv:Private) => {
-        return total + priv.revenue
-      }, 0, player.privates)
+      cash: player.cash + privateRevenue(player)
     }
   default:
     return player
